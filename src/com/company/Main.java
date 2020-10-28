253fdd4af24205b750fc5cb888edb174851dcfd1
iRebeca <birleanurebeca@yahoo.com>
2020-10-28 13:08:44 +0200

    modicari mici

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,8 +13,31 @@ public class Main {
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
     }
+     public static void exempulCuOFB(String mesaj){
+         String iV=initVector(15);
+         KeyManager keyManager= new KeyManager("k1","k2","aesKEy");
+         CommunicationNodeB nodeB= new CommunicationNodeB(keyManager,iV);
+         CommunicationNodeA nodeA= new CommunicationNodeA("OFB",keyManager,nodeB,iV);
 
-    public static void main(String[] args) {
+         /**
+          *  nodul A transmite un mesaj nodului KM prin
+          * care cere cheia corespunzatoare
+          */
+         nodeA.askForCommunicaionKey();
+         /**
+          * nodul A trimite un
+          * mesaj c˘atre nodul B ˆın care comunic˘a modul de operare
+          */
+         nodeA.communicateOperationMode();
+         /**
+          *nodul A cripteaz˘a cont¸inutul unui fi¸sier text utilizˆand AES, cheia primit˘a
+          * de la KM ¸si modul de operare ales. A va transmite nodului B blocurile
+          * de criptotext obt¸inute iar nodul B va decripta blocurile primite ¸si va afi¸sa rezultatul obt¸inut.
+          */
+         nodeA.sendMessageToNodeB(mesaj);
+     }
+
+    public static void exempulCuECB(String mesaj){
         String iV=initVector(15);
         KeyManager keyManager= new KeyManager("k1","k2","aesKEy");
         CommunicationNodeB nodeB= new CommunicationNodeB(keyManager,iV);
@@ -35,6 +58,12 @@ public class Main {
          * de la KM ¸si modul de operare ales. A va transmite nodului B blocurile
          * de criptotext obt¸inute iar nodul B va decripta blocurile primite ¸si va afi¸sa rezultatul obt¸inut.
          */
-        nodeA.sendMessageToNodeB("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+        nodeA.sendMessageToNodeB(mesaj);
+    }
+
+    public static void main(String[] args) {
+       exempulCuECB("um mesaj de pentru care folosim ECB");
+       System.out.println();
+       exempulCuOFB("un mesaj pentru care folosim OFB");
     }
 }
